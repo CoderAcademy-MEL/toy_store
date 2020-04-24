5deac1714ac8cbfd6206e82afaa02fd6048ac954
Harrison Malone <harrison.malone2@gmail.com>
2020-04-24 15:38:07 +1000

    morning challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "clearing out db"
+User.destroy_all
+Toy.destroy_all
 
-for i in 1..20
+puts "starting seed file"
+
+3.times do 
+  Manufacturer.create(
+    name: Faker::TvShows::Seinfeld.business,
+    location: Faker::Nation.capital_city
+  )
+end
+
+3.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: "password"
+  )
+  puts "created new user"
+  random_manufacturer_index = rand(1..3)
+  2.times do 
     Toy.create(
-        name: "Toy#{i}",
-        description: "Toy#{i} is super safe",
-        date_posted: Time.now,
-        posted_by: ["Sally", "Sam", "Tyson", "Mark"].sample
+      name: "Toy #{Toy.count + 1}",
+      description: "Toy #{Toy.count + 1} is super safe",
+      date_posted: Time.now,
+      posted_by: ["Sally", "Sam", "Tyson", "Mark"].sample,
+      user_id: user.id,
+      manufacturer_id: random_manufacturer_index
     )
+    puts "created new toy"
+  end 
 end
